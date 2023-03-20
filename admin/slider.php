998da0965d9d4c4c1f3aeb187cf888ad593c659a
Taikonn09 <126055414+Taikonn09@users.noreply.github.com>
2023-03-20 22:59:32 +0700

    backend

--- admin/slider.php
+++ admin/slider.php
@@ -2,7 +2,7 @@
         <div class="content-left">
             <ul>
                 <li>
-                    <a href="">Danh mục</a>
+                    <a href="#">Danh mục</a>
                     <ul>
                         <li><a href="cartegoryadd.php">Thêm danh mục</a></li>
                         <li><a href="cartegorylist.php">Danh sách danh mục</a></li>
@@ -10,10 +10,10 @@
                 </li>
 
                 <li>
-                    <a href="">Loại SP</a>
+                    <a href="#">Loại SP</a>
                     <ul>
-                        <li><a href="">Thêm loại SP</a></li>
-                        <li><a href="">Danh sách loại SP</a></li>
+                        <li><a href="brandadd.php">Thêm loại SP</a></li>
+                        <li><a href="brandlist.php">Danh sách loại SP</a></li>
                     </ul>
                 </li>
                 <li>
