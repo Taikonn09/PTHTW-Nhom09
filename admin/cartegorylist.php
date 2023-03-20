07be63bb17d1dfa41bcdbcec571cc20f84ed23ae
Taikonn09 <126055414+Taikonn09@users.noreply.github.com>
2023-03-20 13:11:59 +0700

    backend-update

---
+++ admin/cartegorylist.php
@@ -0,0 +1,49 @@
+<?php
+include "header.php";
+include "slider.php";
+include "class/cartegory_class.php"
+?>
+
+<?php
+$cartegory = new cartegory;
+$show_cartegory = $cartegory->show_cartegory();
+?>
+
+<div class="content-right">
+    <div class="content-cartegory-list">
+        <h1>Danh sách danh mục</h1>
+        <table>
+            <tr>
+                <th>STT</th>
+                <th>ID</th>
+                <th>Danh mục</th>
+                <th>Tùy biến</th>
+            </tr>
+
+            <?php
+            if ($show_cartegory) {
+                $i = 0;
+                while ($result = $show_cartegory->fetch_assoc()) {
+                    $i++;
+            ?>
+                    <tr>
+                        <td><?php echo $i ?></td>
+                        <td><?php echo $result['cartegory_id'] ?></td>
+                        <td><?php echo $result['cartegory_name'] ?></td>
+                        <td>
+                            <a href="">Sửa</a>|<a href="">xóa</a>
+                        </td>
+                    </tr>
+
+            <?php
+                }
+            }
+
+            ?>
+        </table>
+    </div>
+</div>
+</section>
+</body>
+
+</html>
---
+++ admin/slider.php
@@ -0,0 +1,27 @@
+<section class="admin-content">
+        <div class="content-left">
+            <ul>
+                <li>
+                    <a href="">Danh mục</a>
+                    <ul>
+                        <li><a href="">Thêm danh mục</a></li>
+                        <li><a href="">Danh sách danh mục</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="">Loại SP</a>
+                    <ul>
+                        <li><a href="">Thêm loại SP</a></li>
+                        <li><a href="">Danh sách loại SP</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="">Sản Phẩm</a>
+                    <ul>
+                        <li><a href="">Thêm Sản Phẩm</a></li>
+                        <li><a href="">Danh sách sản phẩm</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
